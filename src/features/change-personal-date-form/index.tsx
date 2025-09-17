6994aacaebb6a90b88b6eca963a98d5b5d387bf0
Anvarjon Mirzarayimov <anvarjonmirzarayimov@Anvarjons-MacBook-Air.local>
2025-09-17 14:53:05 +0500

    Profile

--- src/features/change-personal-date-form/index.tsx
+++ src/features/change-personal-date-form/index.tsx
@@ -1,6 +1,58 @@
+import { useEffect, useState } from "react";
+import axios from "@/shared/axios";
+
 const ChangePersonalDateForm = () => {
+  const [userData, setUserData] = useState<any>(null);
+  const [fullname, setFullname] = useState("");
+  const [userId, setUserId] = useState<string | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      let storedUserId = localStorage.getItem("user_id");
+
+      if (!storedUserId) {
+        const params = new URLSearchParams(location.search);
+        storedUserId = params.get("user_id");
+
+        if (storedUserId) {
+          localStorage.setItem("user_id", storedUserId);
+        }
+      }
+
+      if (!storedUserId) return;
+      setUserId(storedUserId);
+
+      try {
+        const res = await axios.get(`/profile/users/detail/${storedUserId}/`);
+        setUserData(res.data);
+        setFullname(res.data.fullname || ""); // input ichiga chiqarib qo’yamiz
+      } catch {
+        setUserData(null);
+      }
+    })();
+  }, []);
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!userId) return;
+
+    try {
+      const res = await axios.patch(`/profile/users/detail/${userId}/`, {
+        fullname: fullname,
+      });
+      setUserData(res.data);
+      alert("Full name updated successfully!");
+    } catch (err) {
+      console.error(err);
+      alert("Something went wrong!");
+    }
+  };
+
   return (
-    <form className="mt-10 w-full flex flex-col gap-4">
+    <form
+      onSubmit={handleSubmit}
+      className="mt-10 w-full flex flex-col gap-4"
+    >
       <div>
         <label className="text-sm font-semibold" htmlFor="fullname">
           User Name
@@ -8,22 +60,16 @@ const ChangePersonalDateForm = () => {
         <input
           id="fullname"
           type="text"
+          value={fullname}
+          onChange={(e) => setFullname(e.target.value)}
           className="p-3 border rounded w-full outline-none focus:ring-3 focus:ring-gray-300 transition"
         />
       </div>
 
-      <div>
-        <label className="text-sm font-semibold" htmlFor="number">
-          Phone
-        </label>
-        <input
-          id="number"
-          type="number"
-          className="p-3 border rounded w-full outline-none focus:ring-3 focus:ring-gray-300 transition"
-        />
-      </div>
-
-      <button className="p-4 w-full font-semibold mt-5 rounded-full bg-primary text-white">
+      <button
+        type="submit"
+        className="p-4 w-full font-semibold mt-5 rounded-full bg-primary text-white"
+      >
         Save
       </button>
     </form>
