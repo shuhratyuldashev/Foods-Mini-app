3365234bd010cb6d838ef73bc1d557099f17827e
Anvarjon Mirzarayimov <anvarjonmirzarayimov@Anvarjons-MacBook-Air.local>
2025-09-17 15:37:52 +0500

    last-commit

--- src/pages/for-admins-page/customers-list-page/ui/header-customers-page.tsx
+++ src/pages/for-admins-page/customers-list-page/ui/header-customers-page.tsx
@@ -3,7 +3,6 @@ const HeaderCustomersPage = () => {
     <header className="flex items-center justify-center mt-5">
       <p className="font-semibold">Mijozlar ro'yxati</p>
     </header>
-
   );
 
 };
